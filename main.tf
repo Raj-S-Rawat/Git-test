5a9622f07ead93561216d52938c50e1bed2e2147
rajendra rawat <rsrawat219@gmail.com>
2025-07-07 23:33:54 +0530

    1 new RG code added

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "Raj-rg"{
     name="Raj-rg"
     location= "Central India"
 }
+
+# New Resource group
+
+resource "azurerm_resource_group" "Ratnesh-rg"{
+    name="Ratnesh-rg"
+    location= "Central India"
+}
