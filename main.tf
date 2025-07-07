1dde7cd2c4a4ec7693253513f2fc808baa5651f1
rajendra rawat <rsrawat219@gmail.com>
2025-07-07 23:57:17 +0530

    Dipayan-rg added

--- main.tf
+++ main.tf
@@ -10,10 +10,16 @@ resource "azurerm_storage_account" "SG1" {
   location                 = "Central India"
   account_tier             = "Standard"
   account_replication_type = "GRS"
-
+}
 # New Resource group
 
 resource "azurerm_resource_group" "Ratnesh-rg"{
     name="Ratnesh-rg"
+    location= "Central India"
+}
+
+
+resource "azurerm_resource_group" "Dipayan-rg"{
+    name="Dipayan-rg"
     location= "Central India"
 }
