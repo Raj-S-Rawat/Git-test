f62ea5324345380eb484cd30a48e91c7be627ed6
Raj-S-Rawat <rsrawat2179@gmail.com>
2025-07-07 23:47:02 +0530

    SG code added (#3)
    Co-authored-by: rajendra rawat <rsrawat219@gmail.com>

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ resource "azurerm_resource_group" "Raj-rg"{
     location= "Central India"
 }
 
+# New storage account
+resource "azurerm_storage_account" "SG1" {
+  name                     = "storageaccountname"
+  resource_group_name      = "Raj-rg"
+  location                 = "Central India"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
 # New Resource group
 
 resource "azurerm_resource_group" "Ratnesh-rg"{
